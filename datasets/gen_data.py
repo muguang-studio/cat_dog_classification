3ae487d96bb4a90e01e7abb7d4f7bd79c1331b6a
q651742112 <651742112@qq.com>
2019-05-05 11:46:07 +0800

    init

--- datasets/gen_data.py
+++ datasets/gen_data.py
@@ -102,11 +102,8 @@ def load_and_save(opt, file_name, src_dir, save_dir, split='train'):
 def loading(f_name, src_dir):
     '''加载图片'''
     f_path = os.path.join(src_dir, f_name)
-
-
-
     d = {}
-    #d['image'] = cv2.imread(f_path)
+    # d['image'] = cv2.imread(f_path)
     label, name, suffix = f_name.split('.')
     d['image'] = cv2.imdecode(np.fromfile(f_path, dtype=np.uint8), -1)
     d['label'] = 0 if label == 'cat' else 1
